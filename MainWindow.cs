3fc992a4056588026989034f7ced0041a656a2ec
feather16 <105288087+feather16@users.noreply.github.com>
2023-01-09 17:49:26 +0900

    Add files via upload

---
+++ MainWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public class MainWindow : Form
+    {
+        public MainWindow()
+        {
+			SetStyle(ControlStyles.DoubleBuffer, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            Console.WriteLine("Hello!");
+        }
+    }
+}
